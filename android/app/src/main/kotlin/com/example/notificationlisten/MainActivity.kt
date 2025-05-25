e817e7e57e20da834e70ff90638edc1dff091e93
AissaEl <aissa.root@gmail.com>
2025-05-25 19:34:55 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/notificationlisten/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notificationlisten
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_notification_listener/flutter_notification_listener.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: NotificationsLog(),
+    );
+  }
+}
+
+class NotificationsLog extends StatefulWidget {
+  @override
+  _NotificationsLogState createState() => _NotificationsLogState();
+}
+
+class _NotificationsLogState extends State<NotificationsLog> {
+  List<NotificationEvent> _log = [];
+  bool started = false;
+  bool _loading = false;
+
+  ReceivePort port = ReceivePort();
+
+  @override
+  void initState() {
+    initPlatformState();
+    super.initState();
+  }
+
+  @pragma('vm:entry-point')
+  static void _callback(NotificationEvent evt) {
+    print("send evt to ui: $evt");
+    final SendPort? send = IsolateNameServer.lookupPortByName("_listener_");
+    if (send == null) print("can't find the sender");
+    send?.send(evt);
+  }
+
+  Future<void> initPlatformState() async {
+    NotificationsListener.initialize(callbackHandle: _callback);
+
+    IsolateNameServer.removePortNameMapping("_listener_");
+    IsolateNameServer.registerPortWithName(port.sendPort, "_listener_");
+    port.listen((message) => onData(message));
+
+    var isRunning = (await NotificationsListener.isRunning) ?? false;
+    print("""Service is ${!isRunning ? "not " : ""}already running""");
+
+    setState(() {
+      started = isRunning;
+    });
+  }
+
+  void onData(NotificationEvent event) {
+    setState(() {
+      _log.add(event);
+    });
+
+    print(event.toString());
+  }
+
+  void startListening() async {
+    print("start listening");
+    setState(() {
+      _loading = true;
+    });
+    var hasPermission = (await NotificationsListener.hasPermission) ?? false;
+    if (!hasPermission) {
+      print("no permission, so open settings");
+      NotificationsListener.openPermissionSettings();
+      return;
+    }
+
+    var isRunning = (await NotificationsListener.isRunning) ?? false;
+
+    if (!isRunning) {
+      await NotificationsListener.startService(
+          foreground: false,
+          title: "Listener Running",
+          description: "Welcome to having me");
+    }
+
+    setState(() {
+      started = true;
+      _loading = false;
+    });
+  }
+
+  void stopListening() async {
+    print("stop listening");
+
+    setState(() {
+      _loading = true;
+    });
+
+    await NotificationsListener.stopService();
+
+    setState(() {
+      started = false;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listener Example'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                print("TODO:");
+              },
+              icon: Icon(Icons.settings))
+        ],
+      ),
+      body: Center(
+          child: ListView.builder(
+              itemCount: _log.length,
+              reverse: true,
+              itemBuilder: (BuildContext context, int idx) {
+                final entry = _log[idx];
+                return ListTile(
+                    onTap: () {
+                      entry.tap();
+                    },
+                    title: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(entry.title ?? "<<no title>>"),
+                          Text(entry.text ?? "<<no text>>"),
+                          Text(entry.packageName ?? "<<no text>>"),
+                          Text(entry.createAt.toString().substring(0, 19)),
+                        ],
+                      ),
+                    ));
+              })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: started ? stopListening : startListening,
+        tooltip: 'Start/Stop sensing',
+        child: _loading
+            ? Icon(Icons.close)
+            : (started ? Icon(Icons.stop) : Icon(Icons.play_arrow)),
+      ),
+    );
+  }
+}
